5a175bcb895368bed3ad54ce8364a5cbb411ca6c
josemartinezflorimon <josemartinezflorimon@gmail.com>
2024-10-14 14:35:44 -0400

    feat: Add UserController with getUser, updateUser, and deleteUser functions

---
+++ Api/src/controllers/UserController.js
@@ -0,0 +1,77 @@
+import User from "../models/User";
+
+
+//get user
+const getUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        res.status(200).send({
+            status: 200,
+            message: "User retrieved successfully",
+            user,
+        });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error");
+    }
+}
+
+//update user
+const updateUser = async (req, res) => {
+    try {
+        const { firstName, lastName, email, Image } = req.body;
+
+        const user = await User.findById(req.user.id).select("-password");
+
+        if (!user) {
+            return res.status(404).send({
+                status: 404,
+                message: "User not found",
+            });
+        }
+        
+        user.firstName = firstName;
+        user.lastName = lastName;
+        user.email = email;
+        user.image = Image;
+        
+        await user.save();
+
+        res.status(200).send({
+            status: 200,
+            message: "User updated successfully",
+            user,
+        });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error");
+    }
+}
+
+//delete user
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.user.id);
+
+        if (!user) {
+            return res.status(404).send({
+                status: 404,
+                message: "User not found",
+            });
+        }
+
+        res.status(200).send({
+            status: 200,
+            message: "User deleted successfully",
+        });
+
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error");
+    }
+}
+
+export { getUser, updateUser, deleteUser };
